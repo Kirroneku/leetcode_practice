83d66163e9bac7493229c37e10752fc1c61ba627
Jialin Shan <chansukeizumi@gmail.com>
2020-05-05 12:47:32 -0400

    may_30 days daily

--- Easy_interview_prep/Strings/reverse_int.js
+++ Easy_interview_prep/Strings/reverse_int.js
@@ -3,5 +3,22 @@
  * @return {number}
  */
 var reverse = function(x) {
-    
+    let ans = 0;
+    let neg = (x < 0);
+
+    neg == true? x*=-1: x;
+
+    while( x != 0 ) {
+        ans *= 10;
+        ans += x%10;
+        x = Math.floor(x/10);
+    }
+
+    // Cant overflow
+    if( ans < (-2)**31-1 || ans > 2**31 ) {
+        return 0;
+    }
+
+    return neg == true? ans*-1: ans;
+
 };
