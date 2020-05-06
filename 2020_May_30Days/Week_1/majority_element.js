0c6b1c7865f076860bb51cb2a4bc9c21b83a1d37
Jialin Shan <chansukeizumi@gmail.com>
2020-05-06 13:37:25 -0400

    daily leetcode challenge

--- 2020_May_30Days/Week_1/majority_element.js
+++ 2020_May_30Days/Week_1/majority_element.js
@@ -3,5 +3,28 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    
+    let count = new Map();
+    for( let num of nums ) {
+        if( count.has(num) ) {
+            let counter = count.get(num);
+
+            if( counter+1 > nums.length/2 ) {
+                return num;
+            }
+
+            count.set(num, counter+1);
+        } else {
+            count.set(num, 1);
+        }
+    }
+    let ans = 0;
+    let max = 0;
+    for( let num of count ) {
+        if( num[1] > max ) {
+            max = num[1];
+            ans = num[0];
+        }
+    }
+
+    return ans;
 };
