a1afc97a987f41e6612648ec48b7043970b8d0aa
Jialin Shan <chansukeizumi@gmail.com>
2020-05-06 15:50:19 -0400

    all easy DP qs on leetcode that wasnt done previously

---
+++ Dynamic Programming/Easy/198-house_robber.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function( nums ) {
+    var bestRoute = new Array(nums.length).fill(0);
+    // Recursive option goes over the same nodes a few times
+    var robHelper = function( nums, currentlyRobbing ) {
+        if( currentlyRobbing >= nums.length ) {
+            return 0;
+        } 
+
+        if( bestRoute[currentlyRobbing] != 0 ) {
+            return bestRoute[currentlyRobbing];
+        }
+
+        let best = Math.max(nums[currentlyRobbing] + robHelper(nums, currentlyRobbing+2), 
+                        robHelper(nums, currentlyRobbing+1));
+                        
+        bestRoute[currentlyRobbing] = best;
+        console.log(bestRoute);
+        return best;
+    }
+    
+
+    return robHelper(nums, 0);
+};
+
