405bea50f410dbb223b4f7f9bee66df57e80242f
Jialin Shan <chansukeizumi@gmail.com>
2020-05-13 15:16:22 -0400

    some conditionals

--- 2020_May_30Days/Week_2/remove_k_digits.js
+++ 2020_May_30Days/Week_2/remove_k_digits.js
@@ -10,20 +10,21 @@ var removeKdigits = function(num, k) {
         return '0';
     }
 
-    let buildString = [];
-    let removed = 0;
-
     for( let i = 0; i < k; i++) {
         let j = 0;
-        while( j < numStr.length-1 && numStr[i] <= numStr[i+1] ) {
+        while( j < numStr.length-1 && numStr[j] <= numStr[j+1] ) {
             j++;
         }
 
         numStr.splice(j, 1);
     }
 
-    while( numStr[0] == 0 ) {
+    while( numStr[0] == '0' ) {
         numStr.shift();
+    }
+
+    if( numStr.length == 0 ) {
+        return '0';
     }
 
     return numStr.join("");
