c87e319acad8d6611fd38fb2bb03aa57ffc507c7
Jialin Shan <chansukeizumi@gmail.com>
2020-05-20 15:02:45 -0400

    daily leetcode

---
+++ 2020_May_30Days/Week_3/k-th_smallest_in_bst--naiive.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+    let queue = [root];
+    let sorted = [];
+
+    while( queue.length != 0 ) {
+        let cur = queue.pop();
+
+        if( cur.left != null ) {
+            queue.push(cur.left);
+        }
+
+        if( cur.right != null ){
+            queue.push(cur.right);
+        }
+
+        sorted.push(cur.val);
+    }
+
+    sorted = sorted.sort((a,b) => a-b);
+
+    return sorted[k-1];
+};
+
