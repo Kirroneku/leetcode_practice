6d3bfc5c276a97d6c3bb82dc9e8c873c6ccd7d06
Jialin Shan <chansukeizumi@gmail.com>
2020-05-25 14:52:46 -0400

    Unsaved again :(

--- 2020_May_30Days/Week_4/uncrossed_lines.js
+++ 2020_May_30Days/Week_4/uncrossed_lines.js
@@ -4,33 +4,32 @@
  * @return {number}
  */
 var maxUncrossedLines = function(A, B) {
-    let dp = [];
+    var dp = [];
     for( let i = 0; i < A.length; i ++ ) {
         let newArr = [];
         for( let j = 0; j < B.length; j++) {
-            newArr.push(0);
+            newArr.push(-1);
         }
         dp.push(newArr);
     }
 
     var dpHelper = function( A , B , i , j ) {
+        if( i == A.length || j == B.length ) {
+            return 0;
+        }
 
+        if( dp[i][j] != -1 ) {
+            return dp[i][j];
+        }
+
+        if( A[i] == B[j] ) {
+            dp[i][j] = 1 + dpHelper(A, B, i+1, j+1);
+        } else {
+            dp[i][j] = Math.max(dpHelper( A, B, i+1, j), dpHelper( A, B, i, j+1));
+        }
+        return dp[i][j];
+      
     }
 
-    // let lastA = new Map();
-    // let lastB = new Map();
-
-    // let curIndex = 0;
-    // while( curIndex < Math.max(A.length, B.length) ) {
-
-    //     if( curIndex < A.length ) {
-    //         lastA.set(A[curIndex], curIndex);
-    //     }
-
-    //     if( curIndex < B.length ) {
-    //         lastB.set(B[curIndex], curIndex);
-    //     }
-
-    //     curIndex ++;
-    // }
+    return dpHelper(A, B, 0, 0);
 };
